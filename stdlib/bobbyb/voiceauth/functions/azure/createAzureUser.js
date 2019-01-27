b66bb0dced93f5d72349c182e31240a9f95a13fe
Kieran Haberstock <kieran2401@gmail.com>
2019-01-27 10:26:35 -0800

    Change to verify

--- stdlib/bobbyb/voiceauth/functions/azure/createAzureUser.js
+++ stdlib/bobbyb/voiceauth/functions/azure/createAzureUser.js
@@ -22,7 +22,7 @@ module.exports = (callback) => {
 	      'Ocp-Apim-Subscription-Key': '17d0ba94626641f29ada3e5bdd0d19e5',
 	      'Content-Type': 'application/json'
 	    },
-	    uri: 'https://westus.api.cognitive.microsoft.com/spid/v1.0/identificationProfiles',
+	    uri: 'https://westus.api.cognitive.microsoft.com/spid/v1.0/verificationProfiles',
 	    body: '{"locale":"en-us"}',
 	    method: 'POST'
 	  	}, function (err, res, body) {
